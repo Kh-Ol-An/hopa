db9b8aca990e9a650afe6f40c0456b01ecc054f8
kholan13 <olegkhristenko@gmail.com>
2020-05-06 19:53:35 +0300

    End

--- src/js/lang.js
+++ src/js/lang.js
@@ -2,48 +2,52 @@
 
 const langs = [
   {
-    "lang": "en",
-    "question": "Where is the joker?"
+    lang: 'en',
+    question: 'Where is the joker?',
   },
   {
-    "lang": "de",
-    "question": "Wo ist der Joker?"
+    lang: 'de',
+    question: 'Wo ist der Joker?',
   },
   {
-    "lang": "fi",
-    "question": "Missä on jokeri?"
+    lang: 'fi',
+    question: 'Missä on jokeri?',
   },
   {
-    "lang": "es",
-    "question": "¿Dónde está el Joker?"
+    lang: 'es',
+    question: '¿Dónde está el Joker?',
   },
   {
-    "lang": "no",
-    "question": "Hvor er jokeren?"
-  }
+    lang: 'no',
+    question: 'Hvor er jokeren?',
+  },
 ];
 
-// const language = "gm" // en gm fi es no
-// const currency = "zar" // usd eur gbp zar
+const language = prompt('Введите один из предложеных ниже языков\n\ren     de     fi     es     no\n\rЯзык:');
 
-// const language = prompt('en     gm     fi     es     no\n\rЯзык:');
-// let currency;
-// if (language === 'en') {
-//   currency = prompt('usd     eur     gbp     zar\n\rВалюта:');
-// };
+let currency;
+if (language === 'en') {
+  currency = prompt('Введите одну из предложеных ниже валют\n\rusd     eur     gbp     zar\n\rВалюта:');
+}
 
-let language = window.navigator ? (window.navigator.language || window.navigator.systemLanguage || window.navigator.userLanguage) : "en";
-language = language.substr(0, 2).toLowerCase();
+// let language = window.navigator
+//   ? window.navigator.language ||
+//     window.navigator.systemLanguage ||
+//     window.navigator.userLanguage
+//   : 'en';
+// language = language.substr(0, 2).toLowerCase();
 
-if (language === "nn" || language === "nb") {
-  language = "no";
-} else if (language !== "en" && language !== "de" && language !== "fi" && language !== "es" && language !== "no") {
-  language = "en";
-};
-
-const currency = "usd";
-
-// console.log("language", language)
+// if (language === 'nn' || language === 'nb') {
+//   language = 'no';
+// } else if (
+//   language !== 'en' &&
+//   language !== 'de' &&
+//   language !== 'fi' &&
+//   language !== 'es' &&
+//   language !== 'no'
+// ) {
+//   language = 'en';
+// }
 
 const questionText = document.querySelector('.question__text');
 const findWin = document.querySelector('.find-win_img');
@@ -57,17 +61,19 @@ const rotate = document.querySelector('.rotate-device');
 langs.forEach(lang => {
   if (lang.lang === language) {
     questionText.textContent = lang.question;
-    findWin.setAttribute("src", `./images/${language}/find.png`);
-    mix.setAttribute("src", `./images/${language}/mix.png`);
-    if (language === "en") {
-      won.setAttribute("src", `./images/${language}/${currency}/won.png`);
-      wrong.setAttribute("src", `./images/${language}/${currency}/wrong.png`);
+    findWin.setAttribute('src', `./images/${language}/find.png`);
+    mix.setAttribute('src', `./images/${language}/mix.png`);
+    if (language === 'en') {
+      won.setAttribute('src', `./images/${language}/${currency}/won.png`);
+      wrong.setAttribute('src', `./images/${language}/${currency}/wrong.png`);
     } else {
-      won.setAttribute("src", `./images/${language}/won.png`);
-      wrong.setAttribute("src", `./images/${language}/wrong.png`);
+      won.setAttribute('src', `./images/${language}/won.png`);
+      wrong.setAttribute('src', `./images/${language}/wrong.png`);
     }
-    claims.forEach(claim => claim.setAttribute("src", `./images/${language}/claim.png`))
-    again.setAttribute("src", `./images/${language}/again.png`);
-    rotate.setAttribute("src", `./images/${language}/rotate.png`);
+    claims.forEach(claim =>
+      claim.setAttribute('src', `./images/${language}/claim.png`),
+    );
+    again.setAttribute('src', `./images/${language}/again.png`);
+    rotate.setAttribute('src', `./images/${language}/rotate.png`);
   }
-})
+});
